7cf80870eeae2f1eb59420d143f2d2522a2a60bc
Niko Ekberg <niko.ekberg@gmail.com>
2016-03-19 20:36:08 +0200

    Changed app to use ENV-variables

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,12 +1,8 @@
 var fs = require('fs');
-var env = process.env.NODE_ENV || 'development';
 var path = require('path');
-var config = require(__dirname + '/../config/config.json')[env];
 var basename  = path.basename(module.filename);
 
 var models = {};
-
-var conString = "postgres://"+config.username+":"+config.password+"@"+config.host+":"+config.port+"/"+config.database;
 
 fs
     .readdirSync(__dirname)
@@ -15,7 +11,7 @@ fs
     })
     .forEach(function(file) {
         if (file.slice(-3) !== '.js') return;
-        var model = require(path.join(__dirname, file))(conString);
+        var model = require(path.join(__dirname, file))(process.env.DATABASE_URL);
         models[file.substring(0, file.length - 3)] = model;
     });
 
