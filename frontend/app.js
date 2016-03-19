73e6c65e654e3aa31d5a64fbacb73650eea0282f
Niko Ekberg <niko.ekberg@gmail.com>
2016-03-19 22:06:44 +0200

    Added login code for frontend

---
+++ frontend/app.js
@@ -0,0 +1,52 @@
+/**
+ * Created by ekni on 19/03/16.
+ */
+var app = angular.module('BetMachine', [
+    'ngRoute', 'ngResource'
+]).
+    config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+        $httpProvider.interceptors.push('TokenInterceptor');
+        $routeProvider
+            .when('/', {
+                controller: 'MainController',
+                templateUrl: 'views/main.html',
+                access: {
+                    requireLogin: false,
+                    requireAdmin: false
+                }
+            })
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'views/login.html',
+                access: {
+                    requireLogin: false,
+                    requireAdmin: false
+                }
+            })
+            .otherwise({
+               redirectTo: '/login'
+            });
+}]);
+
+app.run(function($rootScope, $window, $location, AuthenticationFactory) {
+
+    AuthenticationFactory.check();
+
+    $rootScope.$on('$routeChangeStart', function(event, nextRoute, currentRoute) {
+        if((nextRoute.access && nextRoute.access.requireLogin) && !AuthenticationFactory.isLogged) {
+            $location.path('/login');
+        } else {
+            if(!AuthenticationFactory.user) AuthenticationFactory.user = $window.sessionStorage.user;
+            if(!AuthenticationFactory.userRole) AuthenticationFactory.userRole = $window.sessionStorage.userRole;
+        }
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function(event, nextRoute, currentRoute) {
+        $rootScope.role = AuthenticationFactory.userRole;
+
+        if(AuthenticationFactory.isLogged == true && $location.path() == '/login') {
+            $location.path("/");
+        }
+
+    });
+});
---
+++ frontend/controllers/login-controller.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ekni on 19/03/16.
+ */
+app.controller('LoginController', ['$scope', '$window', '$location', 'UserAuthFactory', 'AuthenticationFactory',
+    function ($scope, $window, $location, UserAuthFactory, AuthenticationFactory) {
+        $scope.user = {
+            username: '',
+            password: ''
+        };
+
+        $scope.error = '';
+
+        $scope.login = function () {
+            var username = $scope.user.username,
+                password = $scope.user.password;
+
+            if (username !== undefined && password !== undefined) {
+                UserAuthFactory.login(username, password).success(function (data) {
+
+                    AuthenticationFactory.isLogged = true;
+                    AuthenticationFactory.user = data.user.username;
+                    AuthenticationFactory.userRole = data.user.role;
+
+                    $window.sessionStorage.token = data.token;
+                    $window.sessionStorage.user = data.user.username;
+                    $window.sessionStorage.userRole = data.user.role;
+
+                    $location.path("/");
+
+                }).error(function(status) {
+                    $scope.error = 'Invalid credentials';
+                });
+            } else {
+                $scope.error = 'Please type username and password and try again!';
+            }
+        }
+    }
+]);
