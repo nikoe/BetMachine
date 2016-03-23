6e57cf3337c6b4fac4f45e4bc49a27a12c05b4e4
Niko Ekberg <niko.ekberg@gmail.com>
2016-03-24 00:54:11 +0200

    navbar changes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,11 +4,6 @@ var router = express.Router();
 var userController = require('../controllers/user-controller.js');
 var authController = require('../controllers/auth-controller.js');
 
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost");
-    next();
-});
-
 router.get('/users', [require('../middlewares/validaterequest')], userController.index);
 router.post('/auth', authController.auth);
 
